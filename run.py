a35db13667b6f1f7521058d41983b71caed7d80b
Davor Banovic <davor.banovic@gmail.com>
2019-04-06 21:13:50 +0200

    to_db refoactoring

--- run.py
+++ run.py
@@ -1,1 +1,5 @@
 from to_db import to_db
+
+TD=to_db.ToDb()
+
+TD.to_db()
--- to_db/to_db.py
+++ to_db/to_db.py
@@ -4,24 +4,26 @@ from draw import draw
 from dimensions.sum_of_all_numbers import sum_of_all_numbers
 import pickledb
 
+class ToDb:
+    SH=shuffle.Shuffle()
+    DC= draw.DrawCards()
+    SAN = sum_of_all_numbers.SumOfAllNumbers()
 
-SH=shuffle.Shuffle()
-DC= draw.DrawCards()
-SAN = sum_of_all_numbers.SumOfAllNumbers()
-cards_and_dimensions_db=pickledb.load(config.CARDS_AND_DIMENSIONS_PICKLE_DB, False)
+    def to_db(self):
+        cards_and_dimensions_db = pickledb.load(config.CARDS_AND_DIMENSIONS_PICKLE_DB, False)
 
-i=0
-while i < 3:
+        i=0
+        while i < 3:
 
 
-    drawn_cards = DC.drawCards(SH.shuffleCards(), config.CARDS_TO_DRAW)
-    sum_of_drawn_cards_with_numbers = SAN.sumOfAllNumbers(drawn_cards)
-    drawn_cards.update({"sum_of_drawn_cards_with_numbers": sum_of_drawn_cards_with_numbers})
-    cards_and_dimensions_db.set(str(i), drawn_cards)
+            drawn_cards = self.DC.drawCards(self.SH.shuffleCards(), config.CARDS_TO_DRAW)
+            sum_of_drawn_cards_with_numbers = self.SAN.sumOfAllNumbers(drawn_cards)
+            drawn_cards.update({"sum_of_drawn_cards_with_numbers": sum_of_drawn_cards_with_numbers})
+            cards_and_dimensions_db.set(str(i), drawn_cards)
 
-    i=i+1
+            i=i+1
 
-cards_and_dimensions_db.dump()
-
+        cards_and_dimensions_db.dump()
+        return cards_and_dimensions_db
 
 
