d7447225c686e4e227e331db33d323683f274a29
Davor Banovic <davor.banovic@gmail.com>
2019-04-11 20:26:30 +0200

    Writes csv to file

--- to_csv/to_csv.py
+++ to_csv/to_csv.py
@@ -1,5 +1,18 @@
-#from draw import draw
+from config import config
+import csv
+import pickledb
+cards_and_dimensions_db = pickledb.load(config.CARDS_AND_DIMENSIONS_PICKLE_DB, False)
 
+class ToCsv:
+    def write_csv(self):
+
+        with open(config.CARDS_AND_DIMENSIONS_CSV, mode='w') as csv_file:
+            fieldnames = cards_and_dimensions_db.dkeys("0")
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for i in cards_and_dimensions_db.getall():
+                writer.writerow(cards_and_dimensions_db.dgetall(i))
 
 
 
